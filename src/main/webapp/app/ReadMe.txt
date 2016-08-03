8f6bd1b7399542be67add2a4f9fc5ee2f0e3d243
davisrd <davis_vario@yahoo.com>
2016-08-03 10:38:17 +0700

    hapus view

--- src/main/webapp/app/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-program dapat dijalankan dengan membuka index.html
-untuk beberapa screen sudah berhasil diintegrasikan.
-Namun screen detailReservasi, pemindahanPeminjaman, dan PembatalanPeminjaman masih belum terintegrasi.
--- src/main/webapp/app/js/DaftarPengajuan.js
+++
@@ -1,7 +0,0 @@
-roomReservationControllers.controller('ReservationRequestListController', function($scope) {
-	$scope.listOfReservation = [];
-
-	$scope.selectReservation = function(reservation){
-		$rootScope.selectedReservation = reservation;
-	};
-})
