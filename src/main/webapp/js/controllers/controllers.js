6d39ee510775c3044644f99f206cfac105999867
davisrd <davis_vario@yahoo.com>
2016-08-08 10:42:58 +0700

    view

--- src/main/webapp/js/controllers/controllers.js
+++ src/main/webapp/js/controllers/controllers.js
@@ -4,17 +4,17 @@ simpleApp.controller('inputController',
     function ($scope, $http, $window) {
         $scope.title = "input";
 
-        $scope.jenisKelaminData = [
+        $scope.genderData = [
             { name: 'Pria', value: 1 },
             { name: 'Wanita', value: 2 }
         ];
 
-        $scope.jenisKelaminOptions = {
+        $scope.genderOptions = {
             placeholder: "'Select...'",
             dataTextField: 'name',
             dataValueField: 'value',
             dataSource: {
-                data: $scope.jenisKelaminData
+                data: $scope.genderData
             }
         }
 
@@ -31,4 +31,99 @@ simpleApp.controller('inputController',
                 data: $scope.kelurahanData
             }
         }
+
+        $scope.countryNames = [
+            "Albania",
+            "Andorra",
+            "Armenia",
+            "Austria",
+            "Azerbaijan",
+            "Belarus",
+            "Belgium",
+            "Bosnia & Herzegovina",
+            "Bulgaria",
+            "Croatia",
+            "Cyprus",
+            "Czech Republic",
+            "Denmark",
+            "Estonia",
+            "Finland",
+            "France",
+            "Georgia",
+            "Germany",
+            "Greece",
+            "Hungary",
+            "Iceland",
+            "Ireland",
+            "Italy",
+            "Kosovo",
+            "Latvia",
+            "Liechtenstein",
+            "Lithuania",
+            "Luxembourg",
+            "Macedonia",
+            "Malta",
+            "Moldova",
+            "Monaco",
+            "Montenegro",
+            "Netherlands",
+            "Norway",
+            "Poland",
+            "Portugal",
+            "Romania",
+            "Russia",
+            "San Marino",
+            "Serbia",
+            "Slovakia",
+            "Slovenia",
+            "Spain",
+            "Sweden",
+            "Switzerland",
+            "Turkey",
+            "Ukraine",
+            "United Kingdom",
+            "Vatican City"
+        ];
+
+        $scope.gridData = [
+            {
+                name: "Asep",
+                bornDate: "1988-10-21T13:28:06.419Z",
+                gender: "Pria",
+                joinDate: "1988-10-21T13:28:06.419Z",
+                address: "Bandung"
+
+            },
+            {
+                name: "Hasna",
+                bornDate: "1988-10-21T13:28:06.419Z",
+                gender: "Wanita",
+                joinDate: "1988-10-21T13:28:06.419Z",
+                address: "Bandung"
+            },
+            {
+                name: "Irma",
+                bornDate: "1988-10-21T13:28:06.419Z",
+                gender: "Wanita",
+                joinDate: "1988-10-21T13:28:06.419Z",
+                address: "Bandung"
+            }
+        ];
+
+        $scope.gridColumns = [
+            { field: "name", title: "Nama" },
+            { field: "bornDate", title: "Tanggal Lahir" },
+            { field: "gender", title: "Tanggal Lahir" },
+            { field: "joinDate", title: "Tanggal Masuk" },
+            { field: "address", title: "Alamat" }
+        ];
+        $scope.update = function () {
+            $scope.gridData[0].track = "Hey you";
+            console.log($scope.gridData);
+        };
+        $scope.onChange = function (data) {
+            $scope.selected = data;
+        };
+
+
     });
